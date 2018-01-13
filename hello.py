ce04506d2c1985720b925ff931568222828b3e56
Lewis Blackburn <blackbul@aston.ac.uk>
2018-01-13 17:53:29 +0000

    Added ability to convert postcodes to long/lat co-ordinates.

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, render_template, url_for, redirect, g
 import os
+import urllib2
+import json
 app = Flask(__name__,template_folder='templates')
-print os.environ['MAPPATH']
+api_key = os.environ['MAPPATH']
 
 @app.route("/")
 def hello():
@@ -9,4 +11,13 @@ def hello():
 
 @app.route("/map", methods=['GET', 'POST'])
 def map():
-    return render_template('example.html')
+    # calculates the lat/long given a postcode
+    start = request.form["start"]
+    startPostcode = urllib2.urlopen("https://api.postcodes.io/postcodes/" + start).read()
+    spj = json.loads(startPostcode)
+    startLatLong = {"latitude": spj["result"]["latitude"], "longitude": spj["result"]["longitude"]}
+    finish = request.form["finish"]
+    finishPostcode = urllib2.urlopen("https://api.postcodes.io/postcodes/" + finish).read()
+    fpj = json.loads(finishPostcode)
+    finishLatLong = {"latitude": fpj["result"]["latitude"], "longitude": fpj["result"]["longitude"]}
+    return render_template('example.html', startLatLong=startLatLong, finishLatLong=finishLatLong)
